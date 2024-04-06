41a39af3ed306dd537406ec2956df886066f3481
Moeez <159162874+moeez5251@users.noreply.github.com>
2024-04-06 21:40:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     // For class handling
     var element = document.getElementsByClassName(event.key);
     for (var i = 0; i < element.length; i++) {
         element[i].style.color = 'cyan';
         element[i].style.borderColor = 'cyan';
     }
-
+    //For Id handling
     var elements = document.getElementById(event.key);
     //For Prevent F1-F12 Behaviour
-    if (event.keyCode>= 112 && event.keyCode <= 123) {
-       event.preventDefault();
+    if (event.keyCode >= 112 && event.keyCode <= 123) {
+        event.preventDefault();
     }
     //For Prevent Tab Behaviour
     if (event.keyCode == 9) {
@@ -18,10 +18,14 @@ document.addEventListener('keydown', function(event) {
     if (event.keyCode == 'Meta') {
         event.preventDefault();
     }
-        console.log(event.key);
-        elements.style.color='cyan'
-        elements.style.borderColor='cyan'
-  if(event.getModifierState('CapsLock')){
-    alert('Caps Lock is on.Turn off it.');
-  }
+    // Alt Key Error
+    if (event.keyCode == 18) {
+        event.preventDefault();
+    }
+    if (event.getModifierState('CapsLock')) {
+        alert('Caps Lock is on.Turn off it.');
+    }
+    console.log(event.key);
+    elements.style.color = 'cyan'
+    elements.style.borderColor = 'cyan'
 });
