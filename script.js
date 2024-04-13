e9a264ba92cd7c91598f5ca1bf6a174244e44072
Moeez <159162874+moeez5251@users.noreply.github.com>
2024-04-13 16:10:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener('keydown', function (event) {
     // For class handling
     var element = document.getElementsByClassName(event.key);
-    for (var i = 0; i < element.length; i++) {
-        element[i].style.color = 'cyan';
-        element[i].style.borderColor = 'cyan';
-    }
+    // for (var i = 0; i < element.length; i++) {
+    //     element[i].style.color = 'cyan';
+    //     element[i].style.borderColor = 'cyan';
+    // }
     //For Id handling
     var elements = document.getElementById(event.key);
+
+    //Caps Lock Alert
+    if (event.getModifierState('CapsLock')) {
+        confirm('Caps Lock is on.Turn off it.');
+    }
     //For Prevent F1-F12 Behaviour
     if (event.keyCode >= 112 && event.keyCode <= 123) {
         event.preventDefault();
@@ -22,10 +27,42 @@ document.addEventListener('keydown', function (event) {
     if (event.keyCode == 18) {
         event.preventDefault();
     }
-        if (event.getModifierState('CapsLock')) {
-            alert('Caps Lock is on.Turn off it.');
-        }
+    
+    if (event.key=="Alt"&&event.code=="AltRight") {
+                document.getElementById("Alt-Right").style.color='cyan'
+                document.getElementById("Alt-Right").style.borderColor='cyan'
+            }
+            else if (event.key=="Alt"&&event.code=="AltLeft") {
+                document.getElementById("Alt-Left").style.color='cyan'
+                document.getElementById("Alt-Left").style.borderColor='cyan'
+            }
+            else if (event.key=="Shift"&&event.code=="ShiftLeft") {
+                document.getElementById("shift-left").style.color='cyan'
+       document.getElementById("shift-left").style.borderColor='cyan'
+   }
+            else if (event.key=="Shift"&&event.code=="ShiftRight") {
+                document.getElementById("shift-right").style.color='cyan'
+       document.getElementById("shift-right").style.borderColor='cyan'
+    }
+    else if (event.key=="Control"&&event.code=="ControlLeft") {
+       document.getElementById("Left-C").style.color='cyan'
+   document.getElementById("Left-C").style.borderColor='cyan'
+
+   }
+    else if (event.key=="Control"&&event.code=="ControlRight") {
+       document.getElementById("Right-C").style.color='cyan'
+   document.getElementById("Right-C").style.borderColor='cyan'
+
+   }
+   else if (event.code=="IntlBackslash") {
+    document.getElementById("Slash").style.color='cyan'
+    document.getElementById("Slash").style.borderColor='cyan'
+   }
+   
+   else{
+    elements.style.color='cyan';
+    elements.style.borderColor='cyan';
+   }
+    console.log(event.code);
     console.log(event.key);
-    elements.style.color = 'cyan'
-    elements.style.borderColor = 'cyan'
 });
